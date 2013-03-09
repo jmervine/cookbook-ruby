9905d38fa85a900738a033242f96e979bd726a03
Josh Mervine <joshua@mervine.net>
2013-03-09 14:31:12 -0800

    adding defaults

---
+++ attributes/default.rb
@@ -0,0 +1,6 @@
+default['ruby']['ruby_version']     = "1.9.3-p392"
+default['ruby']['rubygems_version'] = "1.8.24"
+default['ruby']['source_location']  = "http://ftp.ruby-lang.org/pub/ruby/1.9"
+default['ruby']['source_cache_dir'] = "/usr/local/src"
+default['ruby']['destination_dir']  = "/usr/local/bin"
+
